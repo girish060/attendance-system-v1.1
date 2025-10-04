d30715be6265a41d8fa4b249d36b51bc85ccc352
girish060 <girishchaudhary2006@gmail.com>
2025-10-05 01:19:45 +0530

    Added vite.config.ts.timestamp-1759607385056-c05782f7874ff.mjs

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/attendance-system-v1.1/',
+  base: '/college-attendance-system/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
